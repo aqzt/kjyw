03c9348965efe7faa2dac95671f6b6fcf8db0a50
ppabc <ppabc@qq.com>
2016-05-26 09:16:34 +0800

    find

--- find/find.sh
+++ find/find.sh
@@ -13,3 +13,5 @@ find  . -name  "*.rpm" -exec cp {} /root/111 \;
 ##找到*.log日志全部删除
 find . -name *.log | xargs rm
 find . -name *.rpm | xargs rm
+find /data/file1 -name .svn -print0 | xargs -0 rm -r -f
+find /data/file1 -name .git -print0 | xargs -0 rm -r -f
