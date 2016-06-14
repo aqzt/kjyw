16f06a2893361efdb35ccce778994f31763da38b
ppabc <ppabc@qq.com>
2016-06-14 16:27:50 +0800

    kafka

--- kafka/Kafka_zk.sh
+++ kafka/Kafka_zk.sh
@@ -37,18 +37,14 @@ num.recovery.threads.per.data.dir=1
 log.retention.hours=168
 log.segment.bytes=1073741824
 log.retention.check.interval.ms=300000
-zookeeper.connect=master.storm.com:2181/kafka
+zookeeper.connect=master.storm.com:2181
 zookeeper.connection.timeout.ms=6000
 EOF
 
-cat >/opt/zk/conf/zoo.cfg<<EOF
-maxClientCnxns=50
-tickTime=2000
-initLimit=10
-syncLimit=5
+cat >/opt/kafka_2.10-0.9.0.1/config/zookeeper.properties<<EOF
 dataDir=/data/zookeeper/
 clientPort=2181
-server.1=master.storm.com:2888:3888
+maxClientCnxns=50
 EOF
 
 mkdir -p /data/zookeeper/
@@ -64,7 +60,7 @@ cat >/opt/confluent-2.0.1/etc/kafka-rest/kafka-rest.properties<<EOF
 port=8082
 id=kafka-rest-test-server
 schema.registry.url=http://localhost:8081
-zookeeper.connect=127.0.0.1:2181/kafka
+zookeeper.connect=127.0.0.1:2181
 EOF
 
 
@@ -72,30 +68,32 @@ EOF
 ##手动执行
 source  /etc/profile
 
-#需要手动在ZooKeeper中创建路径/kafka，使用如下命令连接到任意一台ZooKeeper服务器：
-cd /opt/zk
-bin/zkCli.sh
-
-#在ZooKeeper执行如下命令创建chroot路径：
-create /kafka ''
 #执行如下命令：
 cd /opt/kafka_2.10-0.9.0.1
+/opt/kafka_2.10-0.9.0.1/bin/zookeeper-server-start.sh /opt/kafka_2.10-0.9.0.1/config/zookeeper.properties   >/dev/null 2>&1 &
+
 /opt/kafka_2.10-0.9.0.1/bin/kafka-server-start.sh /opt/kafka_2.10-0.9.0.1/config/server.properties   >/dev/null 2>&1 &
+
+##bin/zookeeper-server-start.sh config/zookeeper.properties    >/dev/null 2>&1 &
+##bin/kafka-server-start.sh config/server.properties     >/dev/null 2>&1 &
 
 ##停止命令
 ##ps ax | grep -i 'kafka.Kafka' | grep -v grep | awk '{print $1}' | xargs kill
 
 ##查看创建的Topic，执行如下命令：
-bin/kafka-topics.sh --create --zookeeper 192.168.142.136:2181/kafka  --replication-factor 1 --partitions 1 --topic mykafka
+bin/kafka-topics.sh --create --zookeeper 192.168.142.136:2181 --replication-factor 1 --partitions 1 --topic mykafka
+##bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic test
 
 ##列出所有主题
-bin/kafka-topics.sh --list --zookeeper 192.168.142.136:2181/kafka
+bin/kafka-topics.sh --list --zookeeper 192.168.142.136:2181
 
 ##在一个终端，启动Producer，执行如下命令：
 bin/kafka-console-producer.sh --broker-list 192.168.142.136:9092  --topic mykafka
+##bin/kafka-console-producer.sh --broker-list localhost:9092 --topic test 
 
 ##在另一个终端，启动Consumer，执行如下命令：
-bin/kafka-console-consumer.sh --zookeeper 192.168.142.136:2181/kafka --topic mykafka --from-beginning
+bin/kafka-console-consumer.sh --zookeeper 192.168.142.136:2181 --topic mykafka --from-beginning
+##bin/kafka-console-consumer.sh --zookeeper localhost:2181 --topic test --from-beginning
 
 #######confluent启动
 cd /opt/confluent-2.0.1/
