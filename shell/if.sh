9a81beb0634f07fa4188ad0636219af081e6a171
robert <ppabc@qq.com>
2016-07-21 17:38:03 +0800

    close ipv6

---
+++ shell/if.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+## if  2016-07-21
+## http://www.aqzt.com
+##email: ppabc@qq.com
+##robert yu
+##centos 6和centos 7
+
+if [ $b -gt 0 -o $c -gt 0 -a $a -gt 0 ]; then
+echo test
+fi
+#对shell中的关系运算符说明如下：
+#-gt 表示greater than,大于
+#-lt 表示less than,小于
+#-eq 表示 equal,等于
+#对shell中的连接符说明如下：
+#-a 表示 and,且
+#-o 表示 or,或 
+#也可以写成这样：
+if [ $b -gt 0 ] || [ $c -gt 0 ] && [ $a -gt 0 ]; then
+echo test
+fi
+#其中,&&表示and,||表示or
+
+
+#文件表达式
+if [ -f  file ]    #如果文件存在
+if [ -d ...   ]    #如果目录存在
+if [ -s file  ]    #如果文件存在且非空 
+if [ -r file  ]    #如果文件存在且可读
+if [ -w file  ]    #如果文件存在且可写
+if [ -x file  ]    #如果文件存在且可执行   
+
+#整数变量表达式
+if [ int1 -eq int2 ]    #如果int1等于int2   
+if [ int1 -ne int2 ]    #如果不等于    
+if [ int1 -ge int2 ]      # 如果>=
+if [ int1 -gt int2 ]      # 如果>
+if [ int1 -le int2 ]      # 如果<=
+if [ int1 -lt int2 ]      # 如果<
---
+++ shell/ipv6.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+##   2016-07-21
+## http://www.aqzt.com
+##email: ppabc@qq.com
+##robert yu
+##centos 6和centos 7
+
+# /etc/sysctl.conf
+echo "NETWORKING_IPV6=no">>/etc/sysconfig/network
+
+# /etc/sysctl.conf
+cat >>/etc/sysctl.conf<<EOF
+net.ipv6.conf.all.disable_ipv6 = 1
+net.ipv6.conf.default.disable_ipv6 = 1
+net.ipv4.tcp_syn_retries = 1
+net.ipv4.tcp_synack_retries = 1
+net.ipv4.tcp_keepalive_time = 600
+net.ipv4.tcp_keepalive_probes = 3
+net.ipv4.tcp_keepalive_intvl =15
+net.ipv4.tcp_retries2 = 5
+net.ipv4.tcp_fin_timeout = 5
+net.ipv4.tcp_max_tw_buckets = 60000
+net.ipv4.tcp_tw_recycle = 1
+net.ipv4.tcp_tw_reuse = 1
+net.ipv4.tcp_max_orphans = 32768
+net.ipv4.tcp_syncookies = 1
+net.ipv4.tcp_max_syn_backlog = 16384
+net.ipv4.tcp_wmem = 51200 131072 204800
+net.ipv4.tcp_rmem = 51200 131072 204800
+net.ipv4.ip_local_port_range = 1024 65000
+net.core.somaxconn = 16384
+net.core.netdev_max_backlog = 16384
+vm.overcommit_memory = 1
+net.nf_conntrack_max = 6553500
+net.netfilter.nf_conntrack_max = 6553500
+net.netfilter.nf_conntrack_tcp_timeout_established = 180
+EOF
+
+##SSH
+cat >/etc/ssh/sshd_config<<EOF
+cat /etc/ssh/sshd_config
+Port 22
+AddressFamily inet
+ListenAddress 0.0.0.0
+Protocol 2
+SyslogFacility AUTHPRIV
+PermitRootLogin yes
+MaxAuthTries 6
+RSAAuthentication yes
+PubkeyAuthentication yes
+AuthorizedKeysFile	.ssh/authorized_keys
+PasswordAuthentication yes
+PermitEmptyPasswords no
+UsePAM yes
+UseDNS no
+X11Forwarding yes
+Subsystem       sftp    /usr/libexec/openssh/sftp-server
+EOF
+
+#sed -i 's/IPV6INIT=yes/IPV6INIT=no/g' /etc/sysconfig/network-scripts/ifcfg-eth0
+#sed -i 's/IPV6INIT=yes/IPV6INIT=no/g' /etc/sysconfig/network-scripts/ifcfg-eth1
+
+for ((i=0;i<=10;i++))
+do
+if [ -f  /etc/sysconfig/network-scripts/ifcfg-eth$i ]
+        then
+        sed -i 's/IPV6INIT=yes/IPV6INIT=no/g' /etc/sysconfig/network-scripts/ifcfg-eth$i
+                echo $i
+fi
+done
+
+
+
+
+
+
+
+
