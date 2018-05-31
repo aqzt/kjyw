21df61ed5f79718d9134c18a3b89ac4e461cf09b
ppabc <ppabc@qq.com>
2018-05-31 18:05:18 +0800

    update

--- iptables/ipt.sh
+++ iptables/ipt.sh
@@ -7,12 +7,14 @@
 /sbin/iptables -A OUTPUT -j ACCEPT
 /sbin/iptables -A INPUT -s 192.168.1.111 -j ACCEPT
 /sbin/iptables -A INPUT -s 192.168.1.112 -j ACCEPT
-/sbin/iptables -A INPUT  -p tcp --dport 443  -j ACCEPT
+/sbin/iptables -A INPUT  -p tcp --dport 443 -j ACCEPT
 /sbin/iptables -A INPUT  -p tcp --dport 80  -j ACCEPT
+/sbin/iptables -A INPUT  -p tcp --dport 22  -j ACCEPT
 ##允许ping规则
 ##/sbin/iptables -A INPUT  -p icmp  -j ACCEPT
 /sbin/iptables -A INPUT -j REJECT
 /sbin/iptables -A FORWARD -j REJECT
 
 /sbin/service iptables save
+/sbin/iptables  -nL
 echo ok
