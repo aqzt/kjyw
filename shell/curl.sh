991dd8cf3ddf5096e90e372f83121ce5886f5c42
opcache <7529997@qq.com>
2020-01-02 18:56:19 +0800

    curl1

--- shell/curl.sh
+++ shell/curl.sh
@@ -7,7 +7,7 @@
 
 yum install -y bzip2 openssl openssl-devel libcurl
 
-#wget https://github.com/nghttp2/nghttp2/releases/download/v1.40.0/nghttp2-1.40.0.tar.bz2
+wget https://github.com/nghttp2/nghttp2/releases/download/v1.40.0/nghttp2-1.40.0.tar.bz2
 tar xvf nghttp2-1.40.0.tar.bz2
 cd nghttp2-1.40.0
 ./configure
@@ -18,7 +18,7 @@ cd ..
 echo "/usr/local/lib" >> /etc/ld.so.conf
 ldconfig
 
-#wget https://curl.haxx.se/download/curl-7.67.0.tar.gz
+wget https://curl.haxx.se/download/curl-7.67.0.tar.gz
 tar zxvf curl-7.67.0.tar.gz
 cd curl-7.67.0
 ./configure --with-nghttp2=/usr/local --with-ssl
